c9aed12d3d6687fb5d86421fb35f45ec3ac5271a
cyanghxst <155308268+cyanghxst@users.noreply.github.com>
2024-07-16 14:23:23 -0700

    feat(project.js)

--- project.js
+++ project.js
@@ -2,18 +2,18 @@ const $ = id => document.getElementById(id);
 const ctx = $("myCanvas").getContext("2d");
 
 $("myCanvas").addEventListener("click", getCoordinates);
-$("myCanvas").addEventListener("click", drawPieces);
+// $("myCanvas").addEventListener("click", drawPieces);
 // $("myCanvas").addEventListener("click", placePieces);
 
-let row = [];
-row[0] = ["", "red", "", "red", "", "red", "", "red"];
-row[1] = ["red", "", "red", "", "red", "", "red", ""];
-row[2] = ["", "red", "", "red", "", "red", "", "red"];
-row[3] = ["", "", "", "", "", "", "", ""];
-row[4] = ["", "", "", "", "", "", "", ""];
-row[5] = ["grey", "", "grey", "", "grey", "", "grey", ""];
-row[6] = ["", "grey", "", "grey", "", "grey", "", "grey"];
-row[7] = ["grey", "", "grey", "", "grey", "", "grey", ""];
+let array = [];
+array[0] = ["", "red", "", "red", "", "red", "", "red"];
+array[1] = ["red", "", "red", "", "red", "", "red", ""];
+array[2] = ["", "red", "", "red", "", "red", "", "red"];
+array[3] = ["", "", "", "", "", "", "", ""];
+array[4] = ["", "", "", "", "", "", "", ""];
+array[5] = ["grey", "", "grey", "", "grey", "", "grey", ""];
+array[6] = ["", "grey", "", "grey", "", "grey", "", "grey"];
+array[7] = ["grey", "", "grey", "", "grey", "", "grey", ""];
 
 function drawBoard() {
     for (let row = 0; row <= 800; row += 100) {
@@ -39,28 +39,68 @@ function getCoordinates(event) {
     let i = Math.floor(y/100);
 
     console.log(`\nX: ${x}, Y: ${y}`);
-    console.log(row[i][j]);
+    console.log(array[i][j]);
 }
 
 function drawPieces() {
+    for (let row = 0; row <= 800; row += 100) {
+        for (let col = 0; col <= 800; col += 100) {
+            let j = Math.floor(row/100);
+            let i = Math.floor(col/100);
+
+            ctx.beginPath();
+
+            if (array[j][i] == "red") {
+                ctx.fillStyle = "#ff0000";
+            } else if (array[j][i] == "grey") {
+                ctx.fillStyle = "grey";
+            }
+
+            ctx.arc((j * 100) + 50, (i * 100) + 50, 35, 0, 2 * Math.PI);
+            ctx.fill();
+        }
+    }
 
 }
 
+drawPieces();
+
 // function placePieces() {
-//     let x = event.offsetX;
-//     let y = event.offsetY;
+    //     let x = event.offsetX;
+    //     let y = event.offsetY;
 
-//     let j = Math.floor(x/100);
-//     let i = Math.floor(y/100);
+    //     let j = Math.floor(x/100);
+    //     let i = Math.floor(y/100);
 
-//     ctx.beginPath();
+    //     ctx.beginPath();
 
-//     if (row[i][j] == "red") {
-//         ctx.fillStyle = "#ff0000";
-//     } else if (row[i][j] == "grey") {
-//         ctx.fillStyle = "grey";
+    //     if (row[i][j] == "red") {
+        //         ctx.fillStyle = "#ff0000";
+        //     } else if (row[i][j] == "grey") {
+            //         ctx.fillStyle = "grey";
+            //     }
+
+    //     ctx.arc((j * 100) + 50, (i * 100) + 50, 35, 0, 2 * Math.PI);
+    //     ctx.fill();
+    // }
+
+// function drawPieces() {
+//     for (let row = 0; row <= 8; row++) {
+//         for (let col = 0; col <= 8; col++) {
+//             let j = Math.floor(row/100);
+//             let i = Math.floor(col/100);
+
+//             ctx.beginPath();
+
+//             if (array[j][i] == "red") {
+//                 ctx.fillStyle = "#ff0000";
+//             } else if (array[j][i] == "grey") {
+//                 ctx.fillStyle = "grey";
+//             }
+
+//             ctx.arc((j * 100) + 50, (i * 100) + 50, 35, 0, 2 * Math.PI);
+//             ctx.fill();
+//         }
 //     }
 
-//     ctx.arc((j * 100) + 50, (i * 100) + 50, 35, 0, 2 * Math.PI);
-//     ctx.fill();
 // }
